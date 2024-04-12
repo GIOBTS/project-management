ef81b8d6fc112e740056d7672de3e5deb0015eee
Tinsae B <tinsaedev@gmail.com>
2024-04-12 23:12:38 +0300

    Test 8

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -17,7 +17,7 @@ class Dashboard extends BasePage
 {
     protected static bool $shouldRegisterNavigation = false;
 
-    protected static string $view = 'filament.pages.dashboard';
+    protected static string $view = 'filament.pages.dashboard.d';
     protected static ?string $slug = 'dashboard.old';
     
 
